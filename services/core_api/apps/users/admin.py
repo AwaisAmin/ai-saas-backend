d6a9753a95aaf271c06b1336856ba5db921d1b37
Awais <awaisamin090@gmail.com>
2026-03-05 15:53:39 +0500

    implement admin file

--- services/core_api/apps/users/admin.py
+++ services/core_api/apps/users/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_verified', 'created_at')
+    list_filter = ('is_staff', 'is_verified', 'is_active')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('-created_at',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'is_verified')})
+    )
+    add_fieldsets = (
+        (None, {
+            'fields': ('email', 'password1', 'password2')
+        })
+    )
