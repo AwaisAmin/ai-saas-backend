0d4fd99c627600d9622406e24d0a1d41a226f42e
Awais <awaisamin090@gmail.com>
2026-03-16 16:41:15 +0500

    implmented login, register, logout api

--- services/core_api/apps/users/views.py
+++ services/core_api/apps/users/views.py
@@ -1,3 +1,70 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.request import Request
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
+from common.response import success_response, error_response
+from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
+from .services import AuthService, LoginInput, RegisterInput
 
-# Create your views here.
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request: Request):
+        serializer = RegisterSerializer(data=request.data)
+        if not serializer.is_valid():
+            return error_response(errors=serializer.errors, message="Validation failed")
+        
+        data = RegisterInput(
+            email=serializer.validated_data['email'],
+            password=serializer.validated_data['password'],
+            first_name=serializer.validated_data.get('first_name',''),
+            last_name=serializer.validated_data.get('last_name',''),
+        )
+        user = AuthService.register(data)
+
+        return success_response(
+            data=UserSerializer(user).data,
+            message="Account created successfully",
+            status=201
+        )
+    
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request: Request):
+        serializer = LoginSerializer(data=request.data)
+        if not serializer.is_valid():
+            return error_response(errors=serializer.errors, message="Validation failed")
+        
+        data = LoginInput(
+            email=serializer.validated_data['email'],
+            password=serializer.validated_data['password'],
+        )
+        user = AuthService.login(data)
+
+        if user is None:
+            return error_response(message="Invalid email or password", status=401)
+        
+        refresh = RefreshToken.for_user(user)
+        return success_response(
+            data={
+                "user": UserSerializer(user).data,
+                "tokens": {
+                    "access_token": str(refresh.access_token),
+                    "refresh_token": str(refresh),
+                }
+            },
+            message="Login successfully"
+        )
+    
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request: Request):
+        refresh_token = request.data.get("refresh_token")
+        if not refresh_token:
+            return error_response(message="Refresh token required")
+        
+        token = RefreshToken(refresh_token)
+        token.blacklist()
+        return success_response(message="Logged out successfully")
